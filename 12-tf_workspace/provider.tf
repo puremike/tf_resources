197dbfe6419cf7d9312dcd9103e42f8e330f8460
puremike <michaelmarketing25@gmail.com>
2024-08-16 10:12:02 -0500

    used terraform workspace

---
+++ 12-tf_workspace/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "~> 1.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.57.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
---
+++ 12-tf_workspace/s3.tf
@@ -0,0 +1,25 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+/*
+dev     -> 1 bucket
+int     -> 1 bucket
+staging -> 2 buckets
+prod    -> 3 buckets
+*/
+resource "aws_s3_bucket" "this" {
+  count  = var.bucket_count
+  bucket = "workspaces-demo-${terraform.workspace}-${count.index}-${random_id.bucket_suffix.hex}"
+}
+
+
+# Create a new folder to place the files of this section. Configure the Terraform required version and required providers, as well as the AWS provider region.
+
+# Run terraform workspace -help, and have a look through the documentation around CLI workspaces.
+
+# Run terraform workspace show and inspect which workspace we are working with.
+
+# Run terraform workspace list to list all the available workspaces.
+
+# terraform workspace -help
